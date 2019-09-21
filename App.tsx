60d37cdc08bb9f4442466c335a6bc21201be4ba8
user <user@MacBook-Pro-Keizo.local>
2019-09-21 15:43:59 +0900

    create memo list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarText}>MEMO MEMO</Text>
+        </View>
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>アイテム</Text>
+          <Text style={styles.memoDate}>2019/09/25</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddButtonText}>
+          +
+        </Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +33,65 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 70,
   },
+  appbar: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right:0,
+    height: 60,
+    paddingTop: 30,
+    backgroundColor: '#2380BE',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0},
+    shadowOpacity: 0.9,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#FFFDf6',
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2'
+  },
+  memoAddButton: {
+    position: "absolute",
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E86969',
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  memoAddButtonText: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff'
+  }
 });
